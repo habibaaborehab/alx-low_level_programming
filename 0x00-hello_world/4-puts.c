8e2b378a10228e8e5cc2767bdd0066adde42b403
habibaaborehab <habibaaborehab113@gmail.com>
2023-10-05 10:57:46 -0700

    Hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *main - this is the master function
+ *puts - enable us to write a standard output
+ *return - always return 0 means that the code is a success
+*/
+
+int main(void)
+{
+	puts ("/"Programming is like building a multilingual puzzle");
+	return(0);
+}
