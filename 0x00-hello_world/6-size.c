fe8b36f52c4cd72aca9088405110bdbf53a2b1a2
habibaaborehab <habibaaborehab113@gmail.com>
2023-10-05 12:01:25 -0700

    Hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,9 @@
 
 int main(void)
 {
-	printf("Size of a char: %u byte(s)\n", sortof(char));
-	printf("Size of an int: %u byte(s)\n", sortof(int));
-	printf("Size of a long int: %u byte(s)\n", sortof(long int));
-	printf("Size of a long long int: %u byte(s)\n", sortof(long long int));
-	printf("Size of a float: %u byte(s)\n", sortof(float));
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
 }
