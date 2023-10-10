dbc7104f27ea0efa6ad37a7d8efa2b8aa6e5d3cf
habibaaborehab <habibaaborehab113@gmail.com>
2023-10-10 08:04:22 -0700

    task

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,6 +6,5 @@
  * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
-{ return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-	
+{ return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));	
 }
