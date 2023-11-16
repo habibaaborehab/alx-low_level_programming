d89e31fd3d3319fd7622c790b49f9d7b39bac0f5
habibaaborehab <144491243+habibaaborehab@users.noreply.github.com>
2023-11-16 21:26:06 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,7 +18,7 @@ list_t *add_node(list_t **head, const char *str)
 	newHead = malloc(sizeof(list_t));
 	if (newHead == NULL)
 		return (NULL);
-	newHead = strdup(str);
+	newHead->str = strdup(str);
 	if (newHead->str == NULL)
 	{
 		free(newHead);
